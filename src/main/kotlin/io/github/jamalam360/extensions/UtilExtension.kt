555273aa46296ee616e0e8a07a2629bb9ccbd5d7
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-22 19:08:55 +0100

    thread commands

--- src/main/kotlin/io/github/jamalam360/extensions/UtilExtension.kt
+++ src/main/kotlin/io/github/jamalam360/extensions/UtilExtension.kt
@@ -1,10 +1,18 @@
 package io.github.jamalam360.extensions
 
+import com.kotlindiscord.kord.extensions.checks.isInThread
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.threads.edit
+import dev.kord.core.entity.channel.thread.ThreadChannel
 import dev.kord.rest.builder.message.EmbedBuilder
+import io.github.jamalam360.DATABASE
+import kotlinx.coroutines.flow.toList
 
 /**
  * Random commands that don't fit elsewhere.
@@ -36,5 +44,83 @@ class UtilExtension : Extension() {
                 }
             }
         }
+
+        ephemeralSlashCommand {
+            name = "archive"
+            description = "Archive the thread you are in, if you have permission"
+
+            check {
+                isInThread()
+            }
+
+            action {
+                val channel = channel.asChannel() as ThreadChannel
+                val roles = user.asMember(guild!!.id).roles.toList()
+                val modRole = Snowflake(DATABASE.config.getConfig(guild!!.id).moderationConfig.moderatorRole)
+
+                if (roles.contains(guild!!.getRoleOrNull(modRole)) || channel.ownerId == user.id
+                ) {
+                    if (!channel.isArchived) {
+                        channel.edit {
+                            this.archived = true
+                            reason = "Archived by ${user.mention}"
+                        }
+
+                        respond {
+                            content = "Successfully archived thread"
+                        }
+                    } else {
+                        respond {
+                            content = "This thread is already archived"
+                        }
+                    }
+                } else {
+                    respond {
+                        content = "You do not have permission to archive this thread"
+                    }
+                }
+            }
+        }
+
+        ephemeralSlashCommand(::ThreadRenameArgs) {
+            name = "rename"
+            description = "Rename the thread you are in, if you have permission"
+
+            check {
+                isInThread()
+            }
+
+            // Thanks to Cozy for teaching me how to do this
+            action {
+                val channel = channel.asChannel() as ThreadChannel
+                val roles = user.asMember(guild!!.id).roles.toList()
+                val modRole = Snowflake(DATABASE.config.getConfig(guild!!.id).moderationConfig.moderatorRole)
+
+                if (roles.contains(guild!!.getRoleOrNull(modRole)) || channel.ownerId == user.id
+                ) {
+                    channel.edit {
+                        this.name = arguments.name
+                        reason = "Renamed by ${user.mention}"
+                    }
+
+                    respond {
+                        content = "Successfully renamed thread"
+                    }
+                } else {
+                    respond {
+                        content = "You do not have permission to rename this thread"
+                    }
+                }
+            }
+        }
     }
+
+    //region Arguments
+    inner class ThreadRenameArgs : Arguments() {
+        val name by string(
+            "name",
+            "The threads new name"
+        )
+    }
+    //endregion
 }
