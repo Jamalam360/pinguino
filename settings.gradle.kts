07299c1d2732a61ced3f15e5997c00022601f424
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-05 20:57:29 +0000

    drop dependencies back

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         kotlin("plugin.serialization") version "1.5.30"
 
         // Update this in libs.version.toml when you change it here
-        id("io.gitlab.arturbosch.detekt") version "1.18.0"
+        id("io.gitlab.arturbosch.detekt") version "1.18.1"
 
         id("com.github.jakemarsden.git-hooks") version "0.0.1"
         id("com.github.johnrengelman.shadow") version "5.2.0"
